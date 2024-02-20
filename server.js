c45b60b96be98ce31bdc7ddd4503ca83a4c24a41
aniketpethe07 <aniketpethe07@gmail.com>
2024-02-21 03:01:01 +0530

    Added a middleware to parse incoming requests with JSON payloads

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const bodyParser = require("body-parser");
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//Middleware to parse incoming requests with JSON payloads
+app.use(express.json());
+
 // Start the server on port 3000
 app.listen(3000, function () {
   console.log("Server is running on port 3000");
