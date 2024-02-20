4180023a388992351a8569d56cbb406e461718d7
aniketpethe07 <aniketpethe07@gmail.com>
2024-02-21 03:03:53 +0530

    Created a databse connection with MongoDB

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //Middleware to parse incoming requests with JSON payloads
 app.use(express.json());
 
+// Connect to MongoDB
+mongoose.connect("mongodb://localhost:27017/CRUD");
+const db = mongoose.connection;
+
 // Start the server on port 3000
 app.listen(3000, function () {
   console.log("Server is running on port 3000");
